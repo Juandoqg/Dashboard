ebd105b0df876ddd9bd2a13577d79f0be51f0465
Juandoqg <juandoqg@gmail.com>
2024-11-05 23:31:22 -0500

    Sliders y checkbox

--- dashboard.py
+++ dashboard.py
@@ -30,46 +30,70 @@ app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
 app.layout = html.Div([
     html.H1("Dashboard de Docentes", style={'textAlign': 'center', 'color': '#007bff'}),
 
-    # Sección de Consultas Fijas
-    html.Div([
-        dbc.Card([
-            dbc.CardHeader("Consulta 1: Total de Docentes Femeninos con Doctorado en 2014", style={'fontWeight': 'bold'}),
+    # Sección de Consultas Fijas con diseño de cuadrícula
+    dbc.Row([
+        dbc.Col(dbc.Card([
+            dbc.CardHeader("Consulta 1: Docentes Femeninos con Doctorado (2014-2016)", style={'fontWeight': 'bold'}),
             dbc.CardBody([
                 dcc.Graph(id="grafico-consulta-fija-1"),
                 html.Button("Ejecutar Consulta 1", id="btn-consulta-1", className="btn btn-custom"),
             ])
-        ]),
-        dbc.Card([
-            dbc.CardHeader("Consulta 2: Total de Mujeres por Máximo Nivel de Formación", style={'fontWeight': 'bold'}),
+        ]), width=4),
+        
+        dbc.Col(dbc.Card([
+            dbc.CardHeader("Consulta 2: Mujeres por Nivel de Formación", style={'fontWeight': 'bold'}),
             dbc.CardBody([
                 dcc.Graph(id="grafico-consulta-fija-2"),
                 html.Button("Ejecutar Consulta 2", id="btn-consulta-2", className="btn btn-custom"),
             ])
-        ]),
-        dbc.Card([
+        ]), width=4),
+        
+        dbc.Col(dbc.Card([
             dbc.CardHeader("Consulta 3: Total de Docentes por Género", style={'fontWeight': 'bold'}),
             dbc.CardBody([
                 dcc.Graph(id="grafico-consulta-fija-3"),
                 html.Button("Ejecutar Consulta 3", id="btn-consulta-3", className="btn btn-custom"),
             ])
-        ]),
+        ]), width=4),
     ], style={'margin': '20px'}),
 
-    # Sección de Consultas Variables
+    # Sección de Consultas Variables con Dropdown, Slider y Checkboxes
     html.Div([
         html.H3("Consultas Variables"),
-        dcc.Dropdown(
-            id="filtro-consulta-variable",
-            options=[
-                {"label": "Distribución de Docentes por Género y Nivel de Formación", "value": "distribucion_genero_nivel"},
-                {"label": "Total de Docentes por Tipo de Contrato", "value": "total_por_tipo_contrato"},
-                {"label": "Total de Docentes por Municipio", "value": "total_por_municipio"},
-            ],
-            placeholder="Selecciona una consulta",
-            style={'color': '#495057'}
+        dbc.Row([
+            dbc.Col([
+                dcc.Dropdown(
+                    id="filtro-consulta-variable",
+                    options=[
+                        {"label": "Distribución de Docentes por Género y Nivel", "value": "distribucion_genero_nivel"},
+                        {"label": "Total de Docentes por Tipo de Contrato", "value": "total_por_tipo_contrato"},
+                        {"label": "Total de Docentes por Municipio", "value": "total_por_municipio"},
+                    ],
+                    placeholder="Selecciona una consulta",
+                    style={'color': '#495057'}
+                ),
+            ], width=6),
+            dbc.Col([
+                dcc.Checklist(
+                    id="checklist-genero",
+                    options=[
+                        {'label': 'Masculino', 'value': 'MASCULINO'},
+                        {'label': 'Femenino', 'value': 'FEMENINO'},
+                    ],
+                    value=['MASCULINO', 'FEMENINO'],
+                    labelStyle={'display': 'inline-block'}
+                ),
+            ], width=6),
+        ]),
+        dcc.Slider(
+            id="slider-anio",
+            min=df['año'].min(),
+            max=df['año'].max(),
+            marks={str(year): str(year) for year in df['año'].unique()},
+            value=df['año'].min()
         ),
         dcc.Graph(id="grafico-consulta-variable"),
-    ]),
+    ], style={'margin-top': '20px'}),
 
     # Cifras Relevantes
     html.Div(id="cifras-relevantes"),
@@ -86,7 +110,7 @@ app.layout = html.Div([
 )
 def actualizar_graficos_fijos(n1, n2, n3):
     # Consulta 1: Total de Docentes Femeninos con Doctorado en 2014, 2015 y 2016
-    anios = [2021, 2022, 2023]
+    anios = [2021,2022,2023]
     cantidad_femeninos_doctorado = []
 
     for anio in anios:
@@ -96,18 +120,16 @@ def actualizar_graficos_fijos(n1, n2, n3):
         cantidad = total_femeninos_doctorado['numero_docentes'].sum()
         cantidad_femeninos_doctorado.append(cantidad)
 
-    # Cambiar años a cadenas para evitar el problema de los números continuos
-    anios_str = [str(anio) for anio in anios]
-
-    fig1 = px.bar(x=anios_str, y=cantidad_femeninos_doctorado,
-                  title="Cantidad de Docentes Femeninos con Doctorado en 2014, 2015 y 2016",
-                  labels={'x': 'Año', 'y': 'Cantidad de Docentes'},
+    fig1 = px.bar(x=anios, y=cantidad_femeninos_doctorado,
+                  title="Docentes Femeninos con Doctorado (2014-2016)",
+                  labels={'x': 'Año', 'y': 'Cantidad'},
                   text=cantidad_femeninos_doctorado)
     fig1.update_traces(texttemplate='%{text}', textposition='outside')
-    fig1.update_layout(yaxis_title='Cantidad de Docentes', xaxis_title='Año')
-    # Consulta 2: Total de Mujeres por Máximo Nivel de Formación
-    fig2 = px.bar(df[df['genero_docente'] == 'FEMENINO'],
-                  x="maximo_nivel_formacion_docente", y="numero_docentes", title="Total de Mujeres por Máximo Nivel de Formación")
+
+    # Consulta 2: Total de Mujeres por Máximo Nivel de Formación (Gráfico de torta)
+    mujeres_formacion = df[df['genero_docente'] == 'FEMENINO'].groupby('maximo_nivel_formacion_docente').sum().reset_index()
+    fig2 = px.pie(mujeres_formacion, values="numero_docentes", names="maximo_nivel_formacion_docente",
+                  title="Total de Mujeres por Nivel de Formación")
 
     # Consulta 3: Total de Docentes por Género
     fig3 = px.histogram(df, x="genero_docente", y="numero_docentes", 
@@ -118,18 +140,22 @@ def actualizar_graficos_fijos(n1, n2, n3):
 # Callback para las consultas variables
 @app.callback(
     Output("grafico-consulta-variable", "figure"),
-    [Input("filtro-consulta-variable", "value")]
+    [Input("filtro-consulta-variable", "value"),
+     Input("slider-anio", "value"),
+     Input("checklist-genero", "value")]
 )
-def actualizar_grafico_variable(seleccion):
+def actualizar_grafico_variable(seleccion, anio, genero_seleccionado):
+    datos_filtrados = df[(df['año'] == anio) & (df['genero_docente'].isin(genero_seleccionado))]
+
     if seleccion == "distribucion_genero_nivel":
-        fig = px.histogram(df, x="genero_docente", color="maximo_nivel_formacion_docente", 
-                           title="Distribución de Docentes por Género y Máximo Nivel de Formación")
+        fig = px.histogram(datos_filtrados, x="genero_docente", color="maximo_nivel_formacion_docente", 
+                           title="Distribución por Género y Nivel")
     elif seleccion == "total_por_tipo_contrato":
-        fig = px.bar(df, x="tipo_contrato_docente", y="numero_docentes", 
-                     title="Total de Docentes por Tipo de Contrato")
+        fig = px.bar(datos_filtrados, x="tipo_contrato_docente", y="numero_docentes", 
+                     title="Docentes por Tipo de Contrato")
     elif seleccion == "total_por_municipio":
-        fig = px.bar(df, x="municipio_domicilio_ies", y="numero_docentes", 
-                     title="Total de Docentes por Municipio")
+        fig = px.bar(datos_filtrados, x="municipio_domicilio_ies", y="numero_docentes", 
+                     title="Docentes por Municipio")
     else:
         fig = {}
     
@@ -146,15 +172,13 @@ def mostrar_cifras_relevantes(n1, n2, n3):
     total_docentes = df['numero_docentes'].sum()
     total_hombres = df[df['genero_docente'] == 'MASCULINO']['numero_docentes'].sum()
     total_mujeres = df[df['genero_docente'] == 'FEMENINO']['numero_docentes'].sum()
-    promedio_nivel_formacion = df['maximo_nivel_formacion_docente'].mode()[0]  # Modo como promedio representativo
-    total_por_municipio = df.groupby('municipio_domicilio_ies')['numero_docentes'].sum()
+    nivel_formacion_comun = df['maximo_nivel_formacion_docente'].mode()[0]
 
     return [
         html.Div(f"Total de Docentes: {total_docentes}", style={'fontWeight': 'bold'}),
         html.Div(f"Total de Hombres: {total_hombres}", style={'fontWeight': 'bold'}),
         html.Div(f"Total de Mujeres: {total_mujeres}", style={'fontWeight': 'bold'}),
-        html.Div(f"Máximo Nivel de Formación más Común: {promedio_nivel_formacion}", style={'fontWeight': 'bold'}),
-        html.Div(f"Docentes por Municipio: {total_por_municipio.to_dict()}", style={'fontWeight': 'bold'}),
+        html.Div(f"Nivel de Formación más Común: {nivel_formacion_comun}", style={'fontWeight': 'bold'}),
     ]
 
 # Ejecutar la app
